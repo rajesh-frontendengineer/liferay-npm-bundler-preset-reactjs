4d3dd256087f95d71c95f4701b8229e7c2636a62
Rajesh Veldandi <rajesh.veldandi@wnco.com>
2025-01-04 03:49:22 -0600

    feat: v2.0.0 with liferay-preset-2.3.2 versions

---
+++ node_modules/liferay-npm-build-tools-common/lib/api/loaders.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * SPDX-FileCopyrightText: © 2020 Liferay, Inc. <https://liferay.com>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
